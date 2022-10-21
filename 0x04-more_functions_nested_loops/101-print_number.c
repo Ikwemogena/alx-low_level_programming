3e1ec0650e963d8b9ba4b350efcf666ef96aae33
Ikwemogena Abdulai <83832493+Ikwemogena@users.noreply.github.com>
2022-10-21 01:58:29 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,50 +1,22 @@
 #include "main.h"
-
 /**
- * print_number - print number
- * @n: input value
+ * print_number - prints an integer
+ * @n:.input integer parameter
  *
- * Description:  prints an integer
- * Return: Always (0)
+ *
  */
-
 void print_number(int n)
 {
-	if ((n < 0) && (n >= -9))
+	unsigned int i = n;
+
+	if (n < 0)
 	{
-		_putchar('-');
-		_putchar((n * -1) + '0');
+		_putchar(45);
+		i = -i;
 	}
-	else if (n <= -10)
+	if (i / 10)
 	{
-		_putchar('-');
-		_putchar(((n / 10) * -1) + '0');
-		_putchar(((n % 10)  * -1) + '0');
+		print_number(i / 10);
 	}
-	else if (n == 0)
-	{
-		_putchar(n + '0');
-	}
-	else if ((n > 0) && (n <= 9))
-	{
-		_putchar(n + '0');
-	}
-	else if ((n >= 10) && (n <= 99))
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if ((n >= 100) && (n <= 999))
-	{
-		_putchar((n / 100) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if ((n >= 1000) && (n <= 9999))
-	{
-		_putchar((n / 1000) + '0');
-		_putchar((n / 100) % 10 + '0');
-		_putchar((n / 10) % 10 + '0');
-		_putchar((n % 10) + '0');
-	}
+	_putchar(i % 10 + '0');
 }
